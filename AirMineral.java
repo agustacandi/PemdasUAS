c9a6d590f62a83baf36bc14ac4db943c2d5090cc
Candi Agusta <mugiwaraagusta@gmail.com>
2021-12-29 08:51:07 +0700

    Add files via upload

---
+++ AirMineral.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pemdasuas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mugiw
+ */
+public class AirMineral {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(
+"Sudah minum air mineral hari ini?");
+String jawaban = scan.nextLine();
+System.out.println(
+"Sudah minum air mineral hari ini?");
+String jawaban = scan.nextLine();
+    }
+}
