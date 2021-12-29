2bc3674fad1b832f1ec7346148c48ff83d6a731c
Candi Agusta <mugiwaraagusta@gmail.com>
2021-12-29 07:49:56 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pemdasuas;
+
+/**
+ *
+ * @author candiagusta
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Kelas");
+        System.out.println("Nama Saya Candi Agusta Islami Ano");
+    }
+}
